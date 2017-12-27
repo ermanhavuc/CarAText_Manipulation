3ce187b14b8648dc0941c2b56a1d3ac03f55c71f
root <berkayyaman95@gmail.com>
2017-12-27 05:27:12 +0300

    Fontastik wörk2

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <ctype.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <zconf.h>
 
 #define INITIALSIZE 8
 
@@ -17,17 +19,20 @@ typedef struct property{
     struct mainBuffer* bufferPtr;
     size_t used;
     size_t size;
+    int finished,filecount;
 }bufferProperty;
 struct location{
-    char fileName[100];
+    char fileName[256];
     char directoryName[100];
     struct location* locationPtr;
 };
 struct thread_parameters{
-    char * fileName;
-    char * directoryName;
+    char fileName[256];
+    char directoryName[100];
     bufferProperty* bp;
-    int *offsetPtr;
+    size_t offsetPtr;
+    int i,available;
+    pthread_t threadid;
 };
 
 void get_arguments(int argc, char **argv);
@@ -36,29 +41,33 @@ void* work(void*);
 void putWord(char*,struct thread_parameters*);
 bufferProperty* generateBuffer(bufferProperty*);
 void reallocation(bufferProperty*);
-pthread_t thread[2];
+
+sem_t sem,sem2,sem3;
 
 //struct mainBuffer* generateBuffer();
 
 char* directoryName;
 int numberOfThreads = 0,ct=0;
-
 int main(int argc, char **argv) {
     int offsetPtr=0;
+
+    sem_init(&sem, 0, 1);
+    sem_init(&sem2, 0, 1);
+    sem_init(&sem3, 0, 1);
     bufferProperty* bp=malloc(sizeof(bufferProperty));
     get_arguments(argc, argv);  //argumanları çekiyor, hata kontrolü yapabiliriz burada
+
     if(organize(&offsetPtr,generateBuffer(bp))==-1) exit(0);
 }
 
 bufferProperty* generateBuffer(bufferProperty* bp){
-    bp->bufferPtr = malloc(INITIALSIZE*sizeof(struct mainBuffer));
+    bp->bufferPtr = calloc(3,INITIALSIZE*sizeof(struct mainBuffer));
     bp->used=-1;
     bp->size=INITIALSIZE;
 }
 void get_arguments(int argc, char **argv) {
 
     int option;
-
     while ((option = getopt(argc, argv,"d:n:")) != -1) {
         switch (option) {
             case 'd' : directoryName = optarg;
@@ -71,7 +80,7 @@ void get_arguments(int argc, char **argv) {
     }
 }
 int organize(int *offsetPtr,bufferProperty* bp){
-
+    printf("MAIN THREAD: Allocated initial array size to 8 pointers\n");
     DIR *directory;
     directory = opendir(directoryName);
 
@@ -79,93 +88,156 @@ int organize(int *offsetPtr,bufferProperty* bp){
         fprintf(stderr,"Could not open given directory.\n");
         return -1;
     }
-    int i=0;
-    struct dirent *ent;
-    while ((ent = readdir (directory)) != NULL) {
-        struct thread_parameters parameters;
 
-        parameters.directoryName=directoryName;
-        parameters.fileName=ent->d_name;
-        parameters.offsetPtr=offsetPtr;
-        parameters.bp=bp;
-        pthread_create  (&thread[i],  NULL,  &work,  &parameters);
-        i++;
-        if(i==2){
-            pthread_exit(NULL);
+
+    int i;
+    struct dirent *ent;
+    struct thread_parameters parameters[numberOfThreads];
+    for(i=0;i<numberOfThreads;i++){
+        parameters[i].available=1;
+        parameters[i].i=-1;
+    }
+    i=0;
+    pthread_t thread[numberOfThreads];
+    bp->finished=0;
+    bp->filecount=0;
+    while (1) {
+        if(i==numberOfThreads) i=0;
+        if(parameters[i].available==1) {
+            ent = readdir (directory);
+            if(ent==NULL){
+                bp->finished=1;
+                int ct;
+                for(ct=0;ct<numberOfThreads;ct++){
+
+                    pthread_join(parameters[ct].threadid,NULL);
+
+                }
+                printf("MAIN THREAD: All done (successfully read %d words with %d threads from %d files).\n",bp->used,numberOfThreads,bp->filecount);
+                exit(0);
+            }
+            strcpy((parameters[i].directoryName), directoryName);
+            strcpy((parameters[i].fileName), ent->d_name);
+
+            parameters[i].bp = bp;
+
+            parameters[i].available=0;
+            //printf("-----------------%s\n", parameters[i].fileName);
+            if(parameters[i].i==-1) pthread_create(&thread[i], NULL, &work, &parameters[i]);
+
+            parameters[i].i = i;
+            parameters[i].threadid = thread[i];
+
+
         }
+        i++;
     }
 }
 
-void* work(void *parameters){
+void* work(void *parameters) {
     /*Bu üç satırda threadlerin inital fonksiyonlarında kullanılması gereken argümanları
      * pass ettik, bu parametreleri bir struct'ın içine koymuştuk daha öncesinde.
      * struct mainBuffer* bizim üzerine ekleme yaptığımız buffer'ı poin ediyor.
      * */
-    struct thread_parameters* tp=(struct thread_parameters*)parameters;
-    char *fileName = tp->fileName;
-    char *directoryName = tp->directoryName;
+    while(1) {
+        struct thread_parameters *tp = (struct thread_parameters *) parameters;
+        char *fileName = tp->fileName;
+        char *directoryName = tp->directoryName;
+        if (strstr(fileName, ".txt") != NULL) {     //.txt olan bütün dosyaları sırayla çek
+            printf("MAIN THREAD: Assigned \"%s\" to worker thread %u\n",tp->fileName,tp->threadid);
+            sem_wait(&sem3);
+                tp->bp->filecount+=1;
+            sem_post(&sem3);
+            FILE *file;
+            char wordFull[100];
+            char word[100];
+            char *path;
 
-    if(strstr(fileName,".txt") != NULL) {     //.txt olan bütün dosyaları sırayla çek
+            if ((path = malloc(strlen(fileName) + strlen(directoryName) + 2)) != NULL) {   //dosya yolunu oluşturuyor
+                path[0] = '\0';
+                strcat(path, directoryName);
+                strcat(path, "/");
+                strcat(path, fileName);
+            }
 
-        FILE *file;
-        char wordFull[100];
-        char word[100];
-        char *path;
+            file = fopen(path, "r");
 
-        if ((path = malloc(strlen(fileName) + strlen(directoryName) + 2)) != NULL) {   //dosya yolunu oluşturuyor
-            path[0] = '\0';
-            strcat(path, directoryName);
-            strcat(path, "/");
-            strcat(path, fileName);
-        }
+            while (fscanf(file, "%s", wordFull) !=
+                   EOF) {   //kelimeyi çekiyor ve alpha numeric olmayan tüm karakterleri siliyor
 
-        file = fopen(path, "r");
+                int i;
+                int c = 0;
 
-        while (fscanf(file, "%s", wordFull) !=
-               EOF) {   //kelimeyi çekiyor ve alpha numeric olmayan tüm karakterleri siliyor
+                for (i = 0; i < strlen(wordFull); i++) {
 
-            int i;
-            int c = 0;
-
-            for (i = 0; i < strlen(wordFull); i++) {
-
-                if (isalnum(wordFull[i])) {
-                    word[c] = wordFull[i];
-                    c++;
+                    if (isalnum(wordFull[i])) {
+                        word[c] = wordFull[i];
+                        c++;
+                    }
                 }
+                word[c] = '\0';
+                //kelime 'word' elimizde
+
+                sem_wait(&sem);
+                tp->bp->used++;
+                tp->offsetPtr = tp->bp->used;
+                /*printf("Thread:%6d--- used:%6d      size:%6d    word:%6s     filename:%20s\n", tp->i,
+                       tp->bp->used,
+                       tp->bp->size, word, tp->fileName);*/
+                sem_wait(&sem2);
+                if (tp->offsetPtr == tp->bp->size) {
+
+                    reallocation(tp->bp);
+                    printf("THREAD %u: Re-allocated array of %d pointers\n",tp->threadid,tp->bp->size);
+
+                    tp->bp->bufferPtr[tp->offsetPtr].locationHead = NULL;
+
+
+                }
+                sem_post(&sem2);
+                sem_post(&sem);
+
+                sem_wait(&sem2);
+                putWord(word, tp);
+                sem_post(&sem2);
+
             }
-            word[c] = '\0';
-            //kelime 'word' elimizde
-
-
-            tp->bp->used++;
-            if(tp->bp->used==tp->bp->size){
-                reallocation(tp->bp);
-            }
-
-            putWord(word,tp);
-
         }
+        tp->available=1;
+        while(tp->available==1){
+            if(tp->bp->finished==1) {
+                return NULL;
+            }
+        }
+        //printf("asd\n");
     }
 }
 void putWord(char *word,struct thread_parameters* parameters){
-    bufferProperty* bp=parameters->bp;
-    strcpy((bp->bufferPtr)[bp->used].word,word);
-    if(bp->bufferPtr->locationHead==NULL){
-        bp->bufferPtr->locationPtr=malloc(sizeof(struct location));
-        bp->bufferPtr->locationHead=bp->bufferPtr->locationPtr;
-    }else{
-        bp->bufferPtr->locationPtr->locationPtr=malloc(sizeof(struct location));
-        bp->bufferPtr->locationPtr=bp->bufferPtr->locationPtr->locationPtr;
-    }
-    strcpy(bp->bufferPtr->locationPtr->fileName,parameters->fileName);
-    strcpy(bp->bufferPtr->locationPtr->directoryName,parameters->directoryName);
 
-    printf("Thread:%d--- %d word:%4s filename:%4s dname:%4s\n",(int)pthread_self(),bp->size,(bp->bufferPtr)[bp->used].word,bp->bufferPtr->locationPtr->fileName,bp->bufferPtr->locationPtr->directoryName);
-    bp->bufferPtr->locationPtr->fileName;
+    bufferProperty* bp=parameters->bp;
+    //printf("HERE %d\n",parameters->offsetPtr);
+    strcpy((bp->bufferPtr)[parameters->offsetPtr].word,word);
+    if(bp->bufferPtr[parameters->offsetPtr].locationHead==NULL){
+
+        bp->bufferPtr[parameters->offsetPtr].locationPtr=calloc(3,sizeof(struct location));
+        bp->bufferPtr[parameters->offsetPtr].locationHead=bp->bufferPtr->locationPtr;
+    }else{
+
+        bp->bufferPtr[parameters->offsetPtr].locationPtr->locationPtr=calloc(3,sizeof(struct location));
+
+        bp->bufferPtr[parameters->offsetPtr].locationPtr=bp->bufferPtr->locationPtr->locationPtr;
+    }
+
+    strcpy(bp->bufferPtr[parameters->offsetPtr].locationPtr->fileName,parameters->fileName);
+    strcpy(bp->bufferPtr[parameters->offsetPtr].locationPtr->directoryName,parameters->directoryName);
+
+    bp->bufferPtr[parameters->offsetPtr].locationPtr->fileName;
+    //printf("Thread:%6d--- used:%6d    word:%6s     filename:%20s\n",(int)pthread_self(),parameters->offsetPtr,word,parameters->fileName);*/
+    printf("THREAD %u: Added \"%s\" at index %d\n",parameters->threadid,(bp->bufferPtr)[parameters->offsetPtr].word,parameters->offsetPtr);
 }
 void reallocation(bufferProperty *bp){
     bp->size*=2;
     bp->bufferPtr = (struct mainBuffer*)realloc(bp->bufferPtr, bp->size * sizeof(struct mainBuffer));
+
 }
 
